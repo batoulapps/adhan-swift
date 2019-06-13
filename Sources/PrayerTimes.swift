c53aa583b349e26124f355f34605e2d720ed980b
Basem Emara <contact@basememara.com>
2019-06-13 14:58:19 -0400

    Fix comments

--- Sources/PrayerTimes.swift
+++ Sources/PrayerTimes.swift
@@ -142,7 +142,7 @@ public struct PrayerTimes {
         if let maghribAngle = calculationParameters.maghribAngle,
             let maghribComponents = solarTime.timeForSolarAngle(Angle(-maghribAngle), afterTransit: true),
             let maghribDate = cal.date(from: maghribComponents),
-            // maghrib safe if falls between asr and maghrib
+            // maghrib safe if falls between sunset and isha
             sunsetDate.compare(maghribDate) == .orderedAscending,
             (tempIsha?.compare(maghribDate) == .orderedDescending || tempIsha == nil) {
                 tempMaghrib = maghribDate
