7102b1c6def25bd82887958086ad5a58360bedc9
Basem Emara <contact@basememara.com>
2019-06-13 14:49:31 -0400

    Add Tehran time table test

--- AdhanTests/TimeTests.swift
+++ AdhanTests/TimeTests.swift
@@ -34,6 +34,10 @@ class TimeTests: XCTestCase {
             params = CalculationMethod.kuwait.params
         } else if method == "Qatar" {
             params = CalculationMethod.qatar.params
+        } else if method == "Tehran" {
+            params = CalculationMethod.tehran.params
+        } else if method == "Shia" {
+            params = CalculationMethod.shia.params
         } else if method == "Singapore" {
             params = CalculationMethod.singapore.params
         } else {
